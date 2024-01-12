0abbc3cd9bcff3860af64498f6eec3ebc08d8c96
MURSALEEN MOHI-UD-DIN <73093236+MURSALEENMD@users.noreply.github.com>
2024-01-12 11:36:28 +0530

    updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-/*  VaultofCodes Web development Internship Task-2
-    Task assigned on: 12/10/2023
-    Task Deadline: 18/10/2023
-    Task: 
-    Create a portfolio page
-    Component Creation
+/*  
     Description: Build a basic component (e.g., a card, modal, or slider) in your portfolio page using the chosen front-end framework
 
-    Author: Adrian Mathew Aloysius 
+    Author: Mursaleen Mohi-ud-din 
 */
 
 AOS.init();
